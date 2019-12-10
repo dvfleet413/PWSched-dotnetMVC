3f97febe9bff1a81a23d06067c362cca0be90e82
Dave Van Fleet <dvfleet413@gmail.com>
2019-12-09 22:11:18 -0500

    Added support for ASP.NET Core Identity

--- Data/PWSchedContext.cs
+++ Data/PWSchedContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PWSched_dotnet_mvc.Models;
 
 namespace PWSched_dotnet_mvc.Data
 {
-    public class PWSchedContext : DbContext
+    public class PWSchedContext : IdentityDbContext<IdentityUser>
     {
         public PWSchedContext(DbContextOptions<PWSchedContext> options)
             : base(options)
---
+++ Migrations/20191210025047_CreateIdentitySchema.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PWSched_dotnet_mvc.Migrations
+{
+    public partial class CreateIdentitySchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
